151641d15a7ca78c70904e6034a95ffc035da319
TinpotGIT <tinpotgame@gmail.com>
2024-12-23 14:46:23 +0100

    Multiple changes, new popup window, fixes and line breaks working.

--- pydfManip.py
+++ pydfManip.py
@@ -15,6 +15,7 @@ import os
 
 number_of_pages = 0
 filename = ""
+blankPdfName = ""
 text = ""
 errorText = ""
 
@@ -27,8 +28,10 @@ def askFilename():
 def pagecheck():
     T2.configure(state='normal')
     if (filename == ""):
+        T2.configure(state='normal')
         T2.delete(1.0, END)
         T2.insert(END, ("File not given"))
+        T2.configure(state='disabled')
     else:
         if (filename):
             try:
@@ -44,12 +47,37 @@ def pagecheck():
                 T2.delete(1.0, END)
                 T2.insert(END, ("Invalid File, is it a PDF?"))
                 T2.configure(state='disabled')
-
+    
+def popupBlankPDF():
+    global TPop
+    global top
+    top= Toplevel(window)
+    top.geometry("360x140")
+    top.title("Choose the PDF name")
+    
+    Label(top, text= "Choose the PDF Name", font=('Mistral 18 bold')).place(x=40,y=10)
+    
+    TPop = Text(top, height = 1, width = 15)
+    text = ("")
+    TPop.place(relx=0.5, rely=0.55, anchor='s')
+    TPop.insert(END, text)
+    
+    PopButton = Button(top, text ="Save Blank PDF", command = saveBlankPDF)
+    PopButton.place(relx=0.5, rely=0.9, anchor='s')
+    
 def saveBlankPDF():
-    pdf_writer = PdfWriter()
-    page = pdf_writer.add_blank_page(width=8.27 * 72, height=11.7 * 72)
-    with open('BLANK.pdf', 'wb') as file:
-        pdf_writer.write(file)
+    blankPdfName = TPop.get("1.0", 'end-1c')
+    T2.configure(state='normal')
+    if (blankPdfName == ""):
+        T2.configure(state='normal')
+        T2.delete(1.0, END)
+        T2.insert(END, ("PDF name not given"))
+        T2.configure(state='disabled')
+        top.destroy()
+    else:
+        pdfBlank = FPDF()
+        pdfBlank.output(blankPdfName)
+        top.destroy()
 
 def savePDF():
     global text
@@ -64,7 +92,7 @@ def savePDF():
         pdf = FPDF()
         pdf.add_page()
         pdf.set_font("helvetica", style="B", size=16)
-        pdf.cell(40, 10, str(text))
+        pdf.multi_cell(0, None, str(text) + "\n")
         pdf.output("TEXT.pdf")
         T2.configure(state='normal')
         T2.delete(1.0, END)
@@ -74,14 +102,14 @@ def savePDF():
 window = Tk()
 window.geometry("720x480")
 window.resizable(False, False)
-window.title("Pydf - PDF Manipulator")
+window.title("PydfManip - PDF Manipulator")
 B = Button(window, text ="Pages", command = pagecheck)
 B.place(x=50,y=50)
 
 C = Button(window, text ="Filename", command = askFilename)
 C.place(x=600,y=50)
 
-D = Button(window, text ="Create Blank", command = saveBlankPDF)
+D = Button(window, text ="Create Blank", command = popupBlankPDF)
 D.place(x=25,y=150)
 
 E = Button(window, text ="Save Text", command = savePDF)
